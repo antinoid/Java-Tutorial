ac98223968d3b59118b0b1f1b56e7347bbc54139
kothieringer <kothieringer@Kastl-11>
2015-12-08 14:36:58 +0100

    swing animation

--- src/projects/swinganimation/Panel.java
+++ src/projects/swinganimation/Panel.java
@@ -13,40 +13,42 @@ import javax.swing.Timer;
  */
 public class Panel extends JPanel implements ActionListener {
 
+    // Variable, um Zeit zu messen
     private float time = 0;
+    // Swing-Timer
     private Timer timer;
+    // Eigenschaften des Balls ( X- und Y- Koordinate und Radius)
+    private int ballX, ballY;
+    private int ballRadius;
     
     public Panel() {
+        // Hilfsfunktion für den Timer
         initTimer();
+        // Balleigenschaften Werte zuweisen
+        ballX = 200;
+        ballY = 200;
+        ballRadius = 50;
     }
     
+    // unsere eigene Methode, um aufs Panel zu zeichnen
+    // Das Graphics - Objekt g kann man quasi als Pinsel vorstellen
     public void maleAufPanel(Graphics g) {
         
-        int red = (int)time;
-        int green = (int)(time*6);
-        int blue = (int)(time*22);
-        
-        for (int y = 0; y < getHeight(); y++) {
-            
-            for (int x = 0; x < getWidth(); x++) {
-                red += x;
-                green /= x+1;
-                Color color = new Color(red%254, green%255, blue%255);
-                drawPixel(g, x, y, color);
-            }
-        }
+        // Pinsel in die Farbe rot eintauchen
+        g.setColor(Color.red);
+        // Einen Kreis mit dem Pinsel malen
+        g.fillOval(ballX, ballY, ballRadius, ballRadius);
         
         g.dispose();
     }    
     
     private void initTimer() {
+        // Timer Objekt erschaffen
+        // tickt alle 200 Sekunden
+        // Listener für den Timer ist this, da wir ActionListener implementieren
         timer = new Timer(200, this);
+        // Timer starten
         timer.start();
-    }
-    
-    private void drawPixel(Graphics g, int x, int y, Color color) {
-        g.setColor(color);
-        g.drawLine(x, y, x, y);
     }
     
     @Override
@@ -55,10 +57,16 @@ public class Panel extends JPanel implements ActionListener {
         maleAufPanel(g);
     }
     
+    // Wird bei jedem Tick des Timers aufgerufen
     @Override
     public void actionPerformed(ActionEvent e) {
+        // gemessene Zeit erhöhen und ausgeben
         time += 0.2;
-        //System.out.println("time: " + time + " sec");
+        System.out.println("time: " + time + " sec");
+        // Balleigenschaften ändern
+        ballY++;
+        ballX++;
+        // Panel neu zeichnen
         repaint();
     }
 }
